fba2df3c458a6bee54e14e6b4ffe5a87314118f0
Oleg-FFF <foleg555@gmail.com>
2020-04-17 17:04:00 +0300

    go rest api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UserModel} from '../models/UserModel';
 import {UserService} from './services/user.service';
@@ -9,29 +9,17 @@ import {TodoService} from './services/todo.service';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello {{msg}}!</h1>
-            <app-user *ngFor="let u of users" [user]="u"></app-user>
-            <app-post *ngFor="let p of posts" [post]="p"></app-post>
-            <app-todo *ngFor="let t of todos" [todo]="t"></app-todo>
-`,
-  styles: ['h1{background:silver}']
+  templateUrl: './app-component.html',
+  styleUrls: ['./app-component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-dream-app';
   msg: string;
-  users: UserModel[];
-  posts: PostModel[];
-  todos: TodoModel[];
+    constructor(private userService: UserService ) {
 
-  constructor(private userService: UserService, private postService: PostService, private todoService: TodoService) {
-    this.userService.getUsers().subscribe(value => this.users = value);
-    this.postService.getPosts().subscribe(value => this.posts = value);
-    this.todoService.getTodos().subscribe(value => this.todos = value);
-    this.greeteng();
     this.msg = 'user';
   }
-
-  greeteng() {
-    console.log('Hello from here');
-  }
+ngOnInit(): void {
+      this.userService.getUsers().subscribe(value => console.log(value));
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
@@ -8,9 +7,14 @@ import { UserComponent } from './user/user.component';
 import { PostComponent } from './post/post.component';
 import { TodoComponent } from './todo/todo.component';
 import {RouterModule, Routes} from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { AllUsersComponent } from './all-users/all-users.component';
+import { HeaderComponent } from './header/header.component';
+import {UserResolverService} from './services/user-resolver.service';
 
 const routes: Routes = [
-  {path: 'users'}
+  {path: 'users', component: AllUsersComponent, resolve: {allUsers: UserResolverService}},
+  {path: 'hello', component: HelloComponent}
 ]
 
 @NgModule({
@@ -18,7 +22,10 @@ const routes: Routes = [
     AppComponent,
     UserComponent,
     PostComponent,
-    TodoComponent
+    TodoComponent,
+    HelloComponent,
+    AllUsersComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
